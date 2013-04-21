4c808b4059c27c44c041a141d5700d6cb335dd0c
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-04-21 22:59:43 +0700

    Option to use volume keys to control media volume anytime (2/2)
    See Settings part for description
    
    Fix forcing media stream control on tablet
    There's option volume_keys_control_ring_stream (Volume keys control ringtone
    volume / If on, volume keys control ringtone volume. If off, volume keys
    control media volume.) in Settings which is also available on tablets but it
    doesn't work on these devices (I didn't find reported issue).
    As I like this option and think it may be even more wanted on media devices
    such as tablets, here's a fix.
    
    Change-Id: I2f6ebfcbb4ea6e092e4cd29c5acf5f0dbe769718
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3475,6 +3475,13 @@ public final class Settings {
         public static final String RECENTS_FULL_SCREEN = "recents_full_screen";
 
         /**
+         * Volume key controls ringtone or media sound stream
+         * @hide
+         */
+        public static final String VOLUME_KEYS_CONTROL_RING_STREAM =
+                "volume_keys_control_ring_stream";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
