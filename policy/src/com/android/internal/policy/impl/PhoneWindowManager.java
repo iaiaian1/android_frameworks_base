b376de265a218997b3d7dc13a1ee149cfe5e546c
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-04-06 17:50:35 -0500

    Add XPerience message
    Change-Id: Ib315a30a9a66151e16edc376725c0130c3c93a78
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -6588,7 +6588,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     } else {
                         mBootMsgDialog.setTitle(R.string.android_start_title);
                     }
-                    mBootMsgDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+mBootMsgDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		    mBootMsgDialog.setIcon(com.android.internal.R.drawable.xperience);
                     mBootMsgDialog.setIndeterminate(true);
                     mBootMsgDialog.getWindow().setType(
                             WindowManager.LayoutParams.TYPE_BOOT_PROGRESS);
@@ -6602,7 +6603,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     mBootMsgDialog.setCancelable(false);
                     mBootMsgDialog.show();
                 }
-                mBootMsgDialog.setMessage(msg);
+//                mBootMsgDialog.setMessage(msg);
+
+		  mBootMsgDialog.setMessage("Powered By XPerience\n\n" + msg
+		  "\n\nPlease do not power off or remove from power source.");
             }
         });
     }
