61d75ecea21dbfa7ce60fa2779adc0263aa26862
d34d <clark@cyngn.com>
2016-01-14 10:44:38 -0800

    SysUI: Clean up objects when recreating statusbar
    Recreating the status bar for theme changes causes several views
    and objects to not get GCed.  By analyzing hprof dumps of
    com.android.systemui we can identify these issues and get things
    properly cleaned up, and that's what this patch is doing.
    
    Change-Id: Ieaec759e4378a9a7701e6fba46ed33464caaee0a
    TICKET: CYNGNOS-1041
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -71,6 +71,8 @@ public class KeyguardStatusBarView extends RelativeLayout {
         }
     };
 
+    private UserInfoController mUserInfoController;
+
     public KeyguardStatusBarView(Context context, AttributeSet attrs) {
         super(context, attrs);
         showStatusBarCarrier();
@@ -109,6 +111,14 @@ public class KeyguardStatusBarView extends RelativeLayout {
         mCarrierLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                 getResources().getDimensionPixelSize(
                         com.android.internal.R.dimen.text_size_small_material));
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (mUserInfoController != null) {
+            mUserInfoController.removeListener(mUserInfoChangedListener);
+        }
     }
 
     private void loadDimens() {
@@ -165,13 +175,17 @@ public class KeyguardStatusBarView extends RelativeLayout {
         mMultiUserSwitch.setUserSwitcherController(controller);
     }
 
+    private UserInfoController.OnUserInfoChangedListener mUserInfoChangedListener =
+            new UserInfoController.OnUserInfoChangedListener() {
+        @Override
+        public void onUserInfoChanged(String name, Drawable picture) {
+            mMultiUserAvatar.setImageDrawable(picture);
+        }
+    };
+
     public void setUserInfoController(UserInfoController userInfoController) {
-        userInfoController.addListener(new UserInfoController.OnUserInfoChangedListener() {
-            @Override
-            public void onUserInfoChanged(String name, Drawable picture) {
-                mMultiUserAvatar.setImageDrawable(picture);
-            }
-        });
+        mUserInfoController = userInfoController;
+        userInfoController.addListener(mUserInfoChangedListener);
     }
 
     public void setKeyguardUserSwitcher(KeyguardUserSwitcher keyguardUserSwitcher) {
