d8fe9eff4d73c149e9581220387f8a79d0a1af6c
Roman Birg <roman@cyngn.com>
2016-01-20 09:12:45 -0800

    SystemUI: fix disappearing tiles
     (╯°□°）╯︵ ┻━┻
    
    Ref: CYNGNOS-1644
    
    Change-Id: Ib709079b7fc8b8bf54320fc03cee7e1d1b5bbbcc
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1173,6 +1173,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 mBrightnessMirrorController = new BrightnessMirrorController(mStatusBarWindowContent);
             }
             mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
+            mQSPanel.setTiles(mQSTileHost.getTiles());
             mHeader.setQSPanel(mQSPanel);
             mQSTileHost.setCallback(new QSTileHost.Callback() {
                 @Override
@@ -3808,7 +3809,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             @Override
             public void onGlobalLayout() {
                 mStatusBarWindow.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                mQSPanel.setTiles(mQSTileHost.getTiles());
                 mCommandQueue.resume();
                 mRecreating = false;
             }
