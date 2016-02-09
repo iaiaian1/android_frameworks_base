79dc5425b6cdf404f9e85cde77fb981ff3c66a24
Roman Birg <roman@cyngn.com>
2016-02-08 16:02:41 -0800

    SystemUI: fix build
    Change-Id: I8f1d2ab9e1de2408e485dd95c3fb19ae7dfad6f2
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -5247,7 +5247,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             pm.wakeUp(SystemClock.uptimeMillis(), "com.android.systemui:CAMERA_GESTURE");
             mStatusBarKeyguardViewManager.notifyDeviceWakeUpRequested();
         }
-        mWindowManagerService
         vibrateForCameraGesture();
         if (!mStatusBarKeyguardViewManager.isShowing()) {
             startActivity(KeyguardBottomAreaView.INSECURE_CAMERA_INTENT,
