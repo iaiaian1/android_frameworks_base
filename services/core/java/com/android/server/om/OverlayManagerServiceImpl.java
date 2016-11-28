69f14b8c58075cfb9ef550b98ddea9558bd863da
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2016-11-27 20:29:56 -0600

    Revert "OMS-N: Allow multiple targets for enabling and disabling overlays"
    This reverts commit 1666420cd0d9fffc7c6a05d8985ad6e8819d5b52.

--- services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
+++ services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
@@ -312,8 +312,7 @@ class OverlayManagerServiceImpl {
         return mSettings.getOverlaysForUser(userId);
     }
 
-    boolean onSetEnabled(@NonNull String packageName, boolean enable, int userId,
-            boolean shouldWait) {
+    boolean onSetEnabled(@NonNull String packageName, boolean enable, int userId) {
         if (DEBUG) {
             Slog.d(TAG, String.format("onSetEnabled packageName=%s enable=%s userId=%d",
                         packageName, enable, userId));
@@ -328,7 +327,7 @@ class OverlayManagerServiceImpl {
             OverlayInfo oi = mSettings.getOverlayInfo(packageName, userId);
             PackageInfo targetPackage = mPackageManager.getPackageInfo(oi.targetPackageName, userId);
             mSettings.setEnabled(packageName, userId, enable);
-            updateState(targetPackage, overlayPackage, userId, shouldWait);
+            updateState(targetPackage, overlayPackage, userId);
             return true;
         } catch (OverlayManagerSettings.BadKeyException e) {
             return false;
@@ -370,11 +369,6 @@ class OverlayManagerServiceImpl {
 
     private void updateState(PackageInfo targetPackage, @NonNull PackageInfo overlayPackage,
             int userId) throws OverlayManagerSettings.BadKeyException {
-        updateState(targetPackage, overlayPackage, userId, false);
-    }
-
-    private void updateState(PackageInfo targetPackage, @NonNull PackageInfo overlayPackage,
-            int userId, boolean shouldWait) throws OverlayManagerSettings.BadKeyException {
         if (targetPackage != null) {
             mIdmapManager.createIdmap(targetPackage, overlayPackage, userId);
         }
@@ -391,7 +385,7 @@ class OverlayManagerServiceImpl {
                             OverlayInfo.stateToString(currentState),
                             OverlayInfo.stateToString(newState)));
             }
-            mSettings.setState(overlayPackage.packageName, userId, newState, shouldWait);
+            mSettings.setState(overlayPackage.packageName, userId, newState);
         }
     }
 
--- services/core/java/com/android/server/om/OverlayManagerSettings.java
+++ services/core/java/com/android/server/om/OverlayManagerSettings.java
@@ -66,7 +66,7 @@ class OverlayManagerSettings {
         OverlayInfo oi = item.getOverlayInfo();
         mItems.remove(item);
         if (oi != null) {
-            notifyOverlayRemoved(oi, false);
+            notifyOverlayRemoved(oi);
         }
     }
 
@@ -120,7 +120,7 @@ class OverlayManagerSettings {
             OverlayInfo oi = item.getOverlayInfo();
             item.setUpgrading(true);
             item.setState(STATE_NOT_APPROVED_UNKNOWN);
-            notifyOverlayRemoved(oi, false);
+            notifyOverlayRemoved(oi);
             notifySettingsChanged();
         } else {
             item.setUpgrading(false);
@@ -157,8 +157,7 @@ class OverlayManagerSettings {
         return item.getState();
     }
 
-    void setState(@NonNull String packageName, int userId, int state, boolean shouldWait)
-            throws BadKeyException {
+    void setState(@NonNull String packageName, int userId, int state) throws BadKeyException {
         SettingsItem item = select(packageName, userId);
         if (item == null) {
             throw new BadKeyException(packageName, userId);
@@ -167,10 +166,10 @@ class OverlayManagerSettings {
         item.setState(state);
         OverlayInfo current = item.getOverlayInfo();
         if (previous.state == STATE_NOT_APPROVED_UNKNOWN) {
-            notifyOverlayAdded(current, shouldWait);
+            notifyOverlayAdded(current);
             notifySettingsChanged();
         } else if (current.state != previous.state) {
-            notifyOverlayChanged(current, previous, shouldWait);
+            notifyOverlayChanged(current, previous);
             notifySettingsChanged();
         }
     }
@@ -585,32 +584,31 @@ class OverlayManagerSettings {
         }
     }
 
-    private void notifyOverlayAdded(final OverlayInfo oi, boolean shouldWait) {
+    private void notifyOverlayAdded(final OverlayInfo oi) {
         if (DEBUG) {
             assertNotNull(oi);
         }
         for (ChangeListener listener : mListeners) {
-            listener.onOverlayAdded(oi, shouldWait);
+            listener.onOverlayAdded(oi);
         }
     }
 
-    private void notifyOverlayRemoved(final OverlayInfo oi, boolean shouldWait) {
+    private void notifyOverlayRemoved(final OverlayInfo oi) {
         if (DEBUG) {
             assertNotNull(oi);
         }
         for (ChangeListener listener : mListeners) {
-            listener.onOverlayRemoved(oi, shouldWait);
+            listener.onOverlayRemoved(oi);
         }
     }
 
-    private void notifyOverlayChanged(final OverlayInfo oi, final OverlayInfo oldOi,
-            boolean shouldWait) {
+    private void notifyOverlayChanged(final OverlayInfo oi, final OverlayInfo oldOi) {
         if (DEBUG) {
             assertNotNull(oi);
             assertNotNull(oldOi);
         }
         for (ChangeListener listener : mListeners) {
-            listener.onOverlayChanged(oi, oldOi, shouldWait);
+            listener.onOverlayChanged(oi, oldOi);
         }
     }
 
@@ -625,10 +623,9 @@ class OverlayManagerSettings {
 
     interface ChangeListener {
         void onSettingsChanged();
-        void onOverlayAdded(@NonNull OverlayInfo oi, boolean shouldWait);
-        void onOverlayRemoved(@NonNull OverlayInfo oi, boolean shouldWait);
-        void onOverlayChanged(@NonNull OverlayInfo oi, @NonNull OverlayInfo oldOi,
-            boolean shouldWait);
+        void onOverlayAdded(@NonNull OverlayInfo oi);
+        void onOverlayRemoved(@NonNull OverlayInfo oi);
+        void onOverlayChanged(@NonNull OverlayInfo oi, @NonNull OverlayInfo oldOi);
         void onOverlayPriorityChanged(@NonNull OverlayInfo oi);
     }
 
