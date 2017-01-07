55de77da2bcc268ced78ce50184d5eca06d1f1c2
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2017-01-06 22:42:20 -0600

    add missing strings about battery bar

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4038,13 +4038,17 @@ public final class Settings {
         /**
          * @hide
          */
+        public static final String STATUSBAR_BATTERY_BAR_ENABLE_CHARGING_COLOR = "battery_bar_enable_charging_color";
+
+        /**
+         * @hide
+         */
         public static final String STATUSBAR_BATTERY_BAR_BLEND_COLORS = "battery_bar_blend_color";
 
         /**
          * @hide
          */
         public static final String STATUSBAR_BATTERY_BAR_BLEND_COLORS_REVERSE = "battery_bar_blend_color_reverse";
-
         /**
          * These are all public system settings
          *
