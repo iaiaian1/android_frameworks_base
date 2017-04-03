af74a371fa060f87926c9fcaba9d17aefe582c82
Anas Karbila <anaskarbila@gmail.com>
2017-04-03 21:21:36 +0900

    PhoneWindowManager: notify user about assist manager unavailability
    Change-Id: I56957943a8683c763a099b005b72e6ea002dc6e3
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: klozz <klozz707@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -232,6 +232,7 @@ import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.view.autofill.AutofillManagerInternal;
 import android.view.inputmethod.InputMethodManagerInternal;
+import android.widget.Toast;
 
 import com.android.internal.R;
 import com.android.internal.annotations.GuardedBy;
@@ -4350,6 +4351,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             startActivityAsUser(intent, UserHandle.CURRENT);
         } catch (ActivityNotFoundException e) {
             Slog.w(TAG, "No activity to handle assist long press action.", e);
+            Toast.makeText(mContext, R.string.no_assist_found, Toast.LENGTH_SHORT).show();
         }
     }
 
