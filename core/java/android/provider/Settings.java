742b42e78593d7080fede21564c8198f79830880
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-04-14 00:28:15 -0500

    FWB: Woah how (?
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8922,8 +8922,6 @@ public final class Settings {
          */
         public static final String ALARM_MANAGER_CONSTANTS = "alarm_manager_constants";
 
-        public static final String SINGLE_HAND_MODE = "single_hand_mode";
-
         /**
          * @hide
          */
