5b1bfedd5d7ef630b3a3982b720956cc22a5a921
Pranav Vashi <neobuddy89@gmail.com>
2017-05-24 22:53:59 -0500

    SystemUI: Do not use LOS auto brightness implementation
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -51,10 +51,9 @@ import cyanogenmod.providers.CMSettings;
 /** View that represents the quick settings tile panel. **/
 public class QSPanel extends LinearLayout implements Tunable, Callback {
 
-    public static final String QS_SHOW_BRIGHTNESS_SLIDER =
-            "cmsecure:" + CMSettings.Secure.QS_SHOW_BRIGHTNESS_SLIDER;
-    public static final String QS_SHOW_AUTO_BRIGHTNESS =
-            "cmsecure:" + CMSettings.Secure.QS_SHOW_AUTO_BRIGHTNESS;
+    public static final String QS_SHOW_BRIGHTNESS = "qs_show_brightness";
+    public static final String QS_SHOW_BRIGHTNESS_ICON =
+            "system:" + Settings.System.QS_SHOW_BRIGHTNESS_ICON;
 
     protected final Context mContext;
     protected final ArrayList<TileRecord> mRecords = new ArrayList<TileRecord>();
@@ -130,8 +129,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         TunerService.get(mContext).addTunable(this,
-                QS_SHOW_BRIGHTNESS_SLIDER,
-                QS_SHOW_AUTO_BRIGHTNESS);
+                QS_SHOW_BRIGHTNESS,
+                QS_SHOW_BRIGHTNESS_ICON);
         if (mHost != null) {
             setTiles(mHost.getTiles());
         }
@@ -154,13 +153,14 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
 
     @Override
     public void onTuningChanged(String key, String newValue) {
-        if (QS_SHOW_BRIGHTNESS_SLIDER.equals(key)) {
+        if (QS_SHOW_BRIGHTNESS.equals(key)) {
             mBrightnessView.setVisibility(newValue == null || Integer.parseInt(newValue) != 0
                     ? VISIBLE : GONE);
-        } else if (QS_SHOW_AUTO_BRIGHTNESS.equals(key) && mIsAutomaticBrightnessAvailable) {
+        } else if (QS_SHOW_BRIGHTNESS_ICON.equals(key) && mIsAutomaticBrightnessAvailable) {
             mAutoBrightnessView.setVisibility(newValue == null || Integer.parseInt(newValue) != 0
                     ? VISIBLE : GONE);
         }
+       updateResources();
     }
 
     public void openDetails(String subPanel) {
