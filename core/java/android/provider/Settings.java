22dcfc73b6fde10e075d7717459277afc92cb81b
thecrazyskull <anaskarbila@aospa.co>
2018-05-15 11:58:02 -0400

    policy: Add support for device alert sliders.
     * Provide support for alert slider hardware.
    
     * Ability to swap order of alert slider within system setting.
    
     * Enable setting config_hasAlertSlider to true and defining
      proc nodes for state path and UEvent match path.
    
     * Contributors:
    Sirhc95
    ZVNexus
    carlosavignano
    thecrazyskull
    
    Change-Id: I46f594923aedf3445ee9cecb16a90c4963f78a89
    Signed-off-by: klozz <xXx.Reptar.Rawrr.xXx@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4859,12 +4859,21 @@ public final class Settings {
         public static final String PULSE_ON_NEW_TRACKS = "pulse_on_new_tracks";
 
 	/**
+         * Whether user can swap the order of the Alert Slider.
+         * * Whether user can invert the order of the Alert Slider.
+         * 0: Default
+         * 1: Inverted
+         * @hide
+         */
+        public static final String ALERT_SLIDER_ORDER = "alert_slider_order";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
          * the setting value. See an example above.
          */
-        
+
         /**
          * Whether to show the battery info on the lockscreen while charging
          * @hide
