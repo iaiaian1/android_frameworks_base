64aa6e462ca106342f7cd38c9ec13d9f90f40824
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-08-13 12:19:51 -0400

    Shell: Don't show bugreport on DocumentsUI
    Change-Id: I3dde24312f416b1ae2d365fea683aacd8970d923

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1080,6 +1080,7 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     CoreSettingsObserver mCoreSettingsObserver;
 
+/*
     DevelopmentSettingsObserver mDevelopmentSettingsObserver;
 
     private final class DevelopmentSettingsObserver extends ContentObserver {
@@ -1112,7 +1113,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                             : PackageManager.COMPONENT_ENABLED_STATE_DEFAULT,
                     0);
         }
-    }
+    }*/
 
     /**
      * Thread-local storage used to carry caller permissions over through
@@ -7633,7 +7634,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         mConstants.start(mContext.getContentResolver());
         mCoreSettingsObserver = new CoreSettingsObserver(this);
         mActivityTaskManager.installSystemProviders();
-        mDevelopmentSettingsObserver = new DevelopmentSettingsObserver();
+        //mDevelopmentSettingsObserver = new DevelopmentSettingsObserver();
         SettingsToPropertiesMapper.start(mContext.getContentResolver());
         mOomAdjuster.initSettings();
 
