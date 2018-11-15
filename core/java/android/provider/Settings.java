31736f673a2d6e39c3157f0d01c0100315182b8c
klozz <shaclone707@gmail.com>
2018-11-15 07:25:09 -0500

    add missing validators for smartpixels
    Change-Id: I09da128c2114b01d69ee5be4590a63d675810b2f
    Signed-off-by: klozz <shaclone707@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4377,24 +4377,36 @@ public final class Settings {
          * @hide
          */
         public static final String SMART_PIXELS_ENABLE = "smart_pixels_enable";
+        /** @hide */
+        private static final Validator SMART_PIXELS_ENABLE_VALIDATOR =
+                BOOLEAN_VALIDATOR;
 
         /**
          * Smart Pixels pattern
          * @hide
          */
         public static final String SMART_PIXELS_PATTERN = "smart_pixels_pattern";
+        /** @hide */
+        private static final Validator SMART_PIXELS_PATTERN_VALIDATOR =
+                ANY_INTEGER_VALIDATOR;
 
         /**
          * Smart Pixels Shift Timeout
          * @hide
          */
         public static final String SMART_PIXELS_SHIFT_TIMEOUT = "smart_pixels_shift_timeout";
+        /** @hide */
+        private static final Validator SMART_PIXELS_SHIFT_TIMEOUT_VALIDATOR =
+                ANY_INTEGER_VALIDATOR;
 
         /**
          * Whether Smart Pixels should enable on power saver mode
          * @hide
          */
         public static final String SMART_PIXELS_ON_POWER_SAVE = "smart_pixels_on_power_save";
+        /** @hide */
+        private static final Validator SMART_PIXELS_ON_POWER_SAVE_VALIDATOR =
+                BOOLEAN_VALIDATOR;
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
@@ -4457,6 +4469,10 @@ public final class Settings {
             HAPTIC_FEEDBACK_INTENSITY,
             DISPLAY_COLOR_MODE,
             OMNI_LOCKSCREEN_WEATHER_ENABLED,
+            SMART_PIXELS_ENABLE,
+            SMART_PIXELS_PATTERN,
+            SMART_PIXELS_SHIFT_TIMEOUT,
+            SMART_PIXELS_ON_POWER_SAVE,
         };
 
         /**
@@ -4574,6 +4590,10 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(VOLUME_BUTTON_MUSIC_CONTROL);
             PRIVATE_SETTINGS.add(OMNI_LOCKSCREEN_WEATHER_ENABLED);
+            PRIVATE_SETTINGS.add(SMART_PIXELS_ENABLE);
+            PRIVATE_SETTINGS.add(SMART_PIXELS_PATTERN);
+            PRIVATE_SETTINGS.add(SMART_PIXELS_SHIFT_TIMEOUT);
+            PRIVATE_SETTINGS.add(SMART_PIXELS_ON_POWER_SAVE);
         }
 
         /**
@@ -4665,6 +4685,10 @@ public final class Settings {
             VALIDATORS.put(SHOW_BATTERY_PERCENT, SHOW_BATTERY_PERCENT_VALIDATOR);
             VALIDATORS.put(OMNI_LOCKSCREEN_WEATHER_ENABLED,
                     OMNI_LOCKSCREEN_WEATHER_ENABLED_VALIDATOR);
+            VALIDATORS.put(SMART_PIXELS_ENABLE, SMART_PIXELS_ENABLE_VALIDATOR);
+            VALIDATORS.put(SMART_PIXELS_PATTERN, SMART_PIXELS_PATTERN_VALIDATOR);
+            VALIDATORS.put(SMART_PIXELS_SHIFT_TIMEOUT, SMART_PIXELS_SHIFT_TIMEOUT_VALIDATOR);
+            VALIDATORS.put(SMART_PIXELS_ON_POWER_SAVE, SMART_PIXELS_ON_POWER_SAVE_VALIDATOR);
         }
 
         /**
