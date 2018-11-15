b45909648fceeb52a6e4fa9731f88fe841701d1c
klozz <shaclone707@gmail.com>
2018-11-15 09:00:56 -0500

    Revert "AmbientDisplay: Disable hw keys"
    This reverts commit 18a707707563cd3aa862fdf99b8349405ae7b418.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6270,11 +6270,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return 0;
         }
 
-        // Disable hw keys in Ambient
-        if (isDozeMode() && (appSwitchKey || homeKey || menuKey || backKey)) {
-            return 0;
-        }
-
         // Basic policy based on interactive state.
         final boolean isVolumeRockerWake = !isScreenOn()
                 && mVolumeRockerWake
