453f2e95add96d9ab9c5e6859dc77ed522750674
Pranav Vashi <neobuddy89@gmail.com>
2019-02-19 09:59:55 -0300

    ThermalController: Send out active package broadcast only if allowed
    Change-Id: I365e4f13ca90c4ab3f030b2315705a6601eb9c0a
    Signed-off-by: klozz <klozz@vivaldi.net>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -2021,6 +2021,8 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     private static String sTheRealBuildSerial = Build.UNKNOWN;
 
+    final boolean mAllowAppBroadcast;
+
     /**
      * Current global configuration information. Contains general settings for the entire system,
      * also corresponds to the merged configuration of the default display.
@@ -3060,6 +3062,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         mIntentFirewall = null;
         mKeyguardController = null;
         mPermissionReviewRequired = false;
+        mAllowAppBroadcast = false;
         mProcessCpuThread = null;
         mProcessStats = null;
         mProviderMap = null;
@@ -3093,6 +3096,9 @@ public class ActivityManagerService extends IActivityManager.Stub
 
         mPermissionReviewRequired = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_permissionReviewRequired);
+
+        mAllowAppBroadcast = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_allowActivePackageBroadcast);
 
         mHandlerThread = new ServiceThread(TAG,
                 THREAD_PRIORITY_FOREGROUND, false /*allowIo*/);
@@ -27493,6 +27499,10 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     Context getContext() {
         return mContext;
+    }
+
+    public boolean isAppBroadcastAllowed() {
+        return mAllowAppBroadcast;
     }
 
 }
