48658428ddc14fe0c1bc6e70f3ac1968a86550ef
klozz <xXx.Reptar.Rawrr.xXx@gmail.com>
2019-05-12 23:36:21 -0500

    Revert "RecoginitionObserverFactory : Check if we have aquired resources before releasing it."
    This reverts commit dfd86ff837ecd1010c284b8335f7d867278a5dc9.

--- packages/SystemUI/src/com/android/systemui/ambient/play/RecoginitionObserverFactory.java
+++ packages/SystemUI/src/com/android/systemui/ambient/play/RecoginitionObserverFactory.java
@@ -245,7 +245,7 @@ public class RecoginitionObserverFactory extends RecoginitionObserver {
             mRecThread.interrupt();
         }
 
-        if (mRecorder != null && mRecorder.getState() == AudioRecord.STATE_INITIALIZED) {
+        if (mRecorder != null) {
             Log.d(TAG, "Stopping recorder");
             mRecorder.stop();
             mRecorder = null;
