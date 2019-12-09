832db12d839fdee496cc6621aa1bc5291ae23a8b
klozz <klozz@vivaldi.net>
2019-12-09 06:01:07 +0000

    Revert "Add an option to force pre-O apps to use full screen aspect ratio"
    This reverts commit 1c20863c0ccfceb0549c0f0bd4e5352ad7f90ce8.
    
    Change-Id: Ib75923e6f8a74a3e6f357dcfd090f87136562a59
    Signed-off-by: klozz <klozz@vivaldi.net>

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -186,7 +186,6 @@ import android.os.Bundle;
 import android.os.Debug;
 import android.os.IBinder;
 import android.os.Message;
-import android.content.res.Resources;
 import android.os.PersistableBundle;
 import android.os.Process;
 import android.os.RemoteException;
@@ -414,9 +413,6 @@ public final class ActivityRecord extends ConfigurationContainer {
     private boolean mShowWhenLocked;
     private boolean mInheritShownWhenLocked;
     private boolean mTurnScreenOn;
-    // Full screen aspect ratio
-    private final float mFullScreenAspectRatio = Resources.getSystem().getFloat(
-                    com.android.internal.R.dimen.config_screenAspectRatio);
 
     /**
      * Current sequencing integer of the configuration, for skipping old activity configurations.
@@ -3096,9 +3092,7 @@ public final class ActivityRecord extends ConfigurationContainer {
     // TODO(b/36505427): Consider moving this method and similar ones to ConfigurationContainer.
     private void computeBounds(Rect outBounds, Rect containingAppBounds) {
         outBounds.setEmpty();
-        final boolean higherAspectRatio = Resources.getSystem().getBoolean(
-                com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
-        final float maxAspectRatio = higherAspectRatio ? mFullScreenAspectRatio : info.maxAspectRatio;
+        final float maxAspectRatio = info.maxAspectRatio;
         final ActivityStack stack = getActivityStack();
         final float minAspectRatio = info.minAspectRatio;
 
