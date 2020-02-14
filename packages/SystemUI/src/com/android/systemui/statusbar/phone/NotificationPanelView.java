8c9205fbe6e3ec188bd53eb2a1ee84a62cb1fd35
klozz <klozz@vivaldi.net>
2020-02-14 14:51:54 +0000

    fix add quick settings pull down with one finger
    Change-Id: Ief57799411c1fa3f379522751249acba80fd9cb8
    Signed-off-by: klozz <klozz@vivaldi.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1042,7 +1042,7 @@ public class NotificationPanelView extends PanelView implements
             MetricsLogger.count(mContext, COUNTER_PANEL_OPEN_PEEK, 1);
             return true;
         }
-        if (!shouldQuickSettingsIntercept(mDownX, mDownY, 0)
+        if (!shouldQuickSettingsIntercept(mDownX, mDownY, 0, true, false)
                 && mPulseExpansionHandler.onInterceptTouchEvent(event)) {
             return true;
         }
@@ -1251,7 +1251,7 @@ public class NotificationPanelView extends PanelView implements
             expand(true /* animate */);
         }
         initDownStates(event);
-        if (!mIsExpanding && !shouldQuickSettingsIntercept(mDownX, mDownY, 0)
+        if (!mIsExpanding && !shouldQuickSettingsIntercept(mDownX, mDownY, 0, true, false)
                 && mPulseExpansionHandler.onTouchEvent(event)) {
             // We're expanding all the other ones shouldn't get this anymore
             return true;
