73b3463cb08ed6974ec241628d46fe3ed0204fd6
klozz <klozz@vivaldi.net>
2020-02-14 13:46:38 +0000

    fix build in tetheringconfiguration due to modified checkDunRequired from this commit https://github.com/TheXPerienceProject/android_frameworks_base/commit/7279b97924b934f60f2c959621e0945d075fee68
    Change-Id: I015c537ffd947542a75fba05dffbbea7c89c0fb8
    Signed-off-by: klozz <klozz@vivaldi.net>

--- services/core/java/com/android/server/connectivity/tethering/TetheringConfiguration.java
+++ services/core/java/com/android/server/connectivity/tethering/TetheringConfiguration.java
@@ -116,7 +116,7 @@ public class TetheringConfiguration {
         }
         tetherableBluetoothRegexs = getResourceStringArray(res, config_tether_bluetooth_regexs);
 
-        isDunRequired = checkDunRequired(ctx, subId);
+        isDunRequired = checkDunRequired(ctx);
 
         chooseUpstreamAutomatically = getResourceBoolean(res, config_tether_upstream_automatic);
         preferredUpstreamIfaceTypes = getUpstreamIfaceTypes(res, isDunRequired);
