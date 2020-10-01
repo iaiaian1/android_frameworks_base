0731cfc6b0524c576ab5533fd8da85edb75d114e
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-10-01 05:40:28 +0000

    Clocks: SquaryClock
    Htc alike clock
    
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -164,6 +164,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new MNMLMinimalClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new ShapeShiftClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new IDEClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new SquaryClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
