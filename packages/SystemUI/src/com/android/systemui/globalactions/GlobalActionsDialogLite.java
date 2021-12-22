5ead28c9e70e54b023eefa580fca5268f497c4de
Anay Wadhera <anay1018@gmail.com>
2021-12-22 11:58:37 +0700

    [statix-12] base: allow advanced reboot to reboot to fastbootd
    Change-Id: I45dd5c5474742ae1215693b8b93ad151c14a3a4b
    Reviewed-on: https://review.statixos.com/c/android_frameworks_base/+/5800
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Signed-off-by: klozz <klozz@thexperienceproject.org>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -172,6 +172,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
     static final String GLOBAL_ACTION_KEY_SCREENSHOT = "screenshot";
     private static final String GLOBAL_ACTION_KEY_REBOOT_RECOVERY = "reboot_recovery";
     private static final String GLOBAL_ACTION_KEY_REBOOT_BOOTLOADER = "reboot_bootloader";
+    private static final String GLOBAL_ACTION_KEY_REBOOT_FASTBOOT = "reboot_fastboot";
 
     private final Context mContext;
     private final GlobalActionsManager mWindowManagerFuncs;
@@ -589,13 +590,18 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
 
         String[] rebootMenuActions = mContext.getResources().getStringArray(
                     com.android.internal.R.array.config_rebootActionsList);
+        boolean arEnabled = advancedRebootEnabled(mContext);
         for (String actionKey : rebootMenuActions) {
-            if (advancedRebootEnabled(mContext) && GLOBAL_ACTION_KEY_REBOOT_RECOVERY.equals(actionKey)) {
-                RebootRecoveryAction a = new RebootRecoveryAction();
-                addIfShouldShowAction(items, a);
-            } else if (advancedRebootEnabled(mContext) && GLOBAL_ACTION_KEY_REBOOT_BOOTLOADER.equals(actionKey)) {
-                RebootBootloaderAction a = new RebootBootloaderAction();
-                addIfShouldShowAction(items, a);
+            if (arEnabled) {
+                if (GLOBAL_ACTION_KEY_REBOOT_RECOVERY.equals(actionKey)) {
+                    RebootRecoveryAction a = new RebootRecoveryAction();
+                    addIfShouldShowAction(items, a);
+                } else if (GLOBAL_ACTION_KEY_REBOOT_BOOTLOADER.equals(actionKey)) {
+                    RebootBootloaderAction a = new RebootBootloaderAction();
+                    addIfShouldShowAction(items, a);
+                } else if (GLOBAL_ACTION_KEY_REBOOT_FASTBOOT.equals(actionKey)) {
+                    addIfShouldShowAction(items, new RebootFastbootAction());
+                }
             }
         }
         return items;
@@ -611,6 +617,14 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                 && actions.size() > getMaxShownPowerItems();
     }
 
+    private boolean isFastbootAvailable() {
+        final boolean dynamic = SystemProperties.getBoolean("ro.boot.dynamic_partitions", false);
+        final boolean retrofit = SystemProperties.getBoolean("ro.boot.dynamic_partitions_retrofit", false);
+        final boolean overrideFastboot = SystemProperties.getBoolean("ro.fastbootd.available", false);
+        return dynamic || retrofit || overrideFastboot;
+    }
+
+
     private void buildMenuList() {
         List<Action> tempActions = new ArrayList<>();
 
@@ -620,6 +634,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         RestartAction restartAction = new RestartAction();
         RebootRecoveryAction rbtRecoveryAction = new RebootRecoveryAction();
         RebootBootloaderAction rbtBlAction = new RebootBootloaderAction();
+        RebootFastbootAction rbtFbAction = new RebootFastbootAction();
 
         // make sure emergency affordance action is first, if needed
         if (mEmergencyAffordanceManager.needsEmergencyAffordance() && !mRebootMenu) {
@@ -666,6 +681,8 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                 addIfShouldShowAction(tempActions, rbtRecoveryAction);
             } else if (advancedRebootEnabled(mContext) && GLOBAL_ACTION_KEY_REBOOT_BOOTLOADER.equals(actionKey)) {
                 addIfShouldShowAction(tempActions, rbtBlAction);
+            } else if (advancedRebootEnabled(mContext) && GLOBAL_ACTION_KEY_REBOOT_FASTBOOT.equals(actionKey) && isFastbootAvailable()) {
+                addIfShouldShowAction(tempActions, rbtFbAction);
             } else if (GLOBAL_ACTION_KEY_SCREENSHOT.equals(actionKey)) {
                 addIfShouldShowAction(tempActions, new ScreenshotAction());
             } else if (GLOBAL_ACTION_KEY_LOGOUT.equals(actionKey)) {
@@ -700,6 +717,9 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
             if (advancedRebootEnabled(mContext)) {
                 mPowerItems.add(rbtRecoveryAction);
                 mPowerItems.add(rbtBlAction);
+                if (isFastbootAvailable()) {
+                    mPowerItems.add(rbtFbAction);
+                }
             }
 
             // add the PowerOptionsAction after Emergency, if present
@@ -1054,6 +1074,33 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         @Override
         public void onPress() {
             mWindowManagerFuncs.reboot(false, PowerManager.REBOOT_BOOTLOADER);
+        }
+    }
+
+    final class RebootFastbootAction extends SinglePressAction {
+        RebootFastbootAction() {
+            super(com.android.systemui.R.drawable.ic_lock_restart_fastboot,
+                    com.android.systemui.R.string.global_action_reboot_fastboot);
+        }
+
+        @Override
+        public boolean showDuringKeyguard() {
+            return true;
+        }
+
+        @Override
+        public boolean showDuringRestrictedKeyguard() {
+            return false;
+        }
+
+        @Override
+        public boolean showBeforeProvisioning() {
+            return true;
+        }
+
+        @Override
+        public void onPress() {
+            mWindowManagerFuncs.reboot(false, PowerManager.REBOOT_FASTBOOT);
         }
     }
 
